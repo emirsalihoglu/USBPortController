5879730aadaeaef026945ddf4f31a513923aed4f
memir <memir@DESKTOP-8V6HB53>
2023-07-29 15:42:18 +0300

    Proje dosyası ekle.

---
+++ Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace USBPortController
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonEnable = new System.Windows.Forms.Button();
+            this.buttonDisable = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.buttonCheckDevices = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonEnable
+            // 
+            this.buttonEnable.Location = new System.Drawing.Point(12, 197);
+            this.buttonEnable.Name = "buttonEnable";
+            this.buttonEnable.Size = new System.Drawing.Size(109, 23);
+            this.buttonEnable.TabIndex = 0;
+            this.buttonEnable.Text = "Etkinleştir";
+            this.buttonEnable.UseVisualStyleBackColor = true;
+            this.buttonEnable.Click += new System.EventHandler(this.buttonEnable_Click);
+            // 
+            // buttonDisable
+            // 
+            this.buttonDisable.Location = new System.Drawing.Point(127, 197);
+            this.buttonDisable.Name = "buttonDisable";
+            this.buttonDisable.Size = new System.Drawing.Size(109, 23);
+            this.buttonDisable.TabIndex = 1;
+            this.buttonDisable.Text = "Devre Dışı Bırak";
+            this.buttonDisable.UseVisualStyleBackColor = true;
+            this.buttonDisable.Click += new System.EventHandler(this.buttonDisable_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(386, 160);
+            this.listBox1.TabIndex = 2;
+            // 
+            // buttonCheckDevices
+            // 
+            this.buttonCheckDevices.Location = new System.Drawing.Point(289, 197);
+            this.buttonCheckDevices.Name = "buttonCheckDevices";
+            this.buttonCheckDevices.Size = new System.Drawing.Size(109, 23);
+            this.buttonCheckDevices.TabIndex = 3;
+            this.buttonCheckDevices.Text = "Cihazları Göster";
+            this.buttonCheckDevices.UseVisualStyleBackColor = true;
+            this.buttonCheckDevices.Click += new System.EventHandler(this.buttonCheckDevices_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(423, 244);
+            this.Controls.Add(this.buttonCheckDevices);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.buttonDisable);
+            this.Controls.Add(this.buttonEnable);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonEnable;
+        private System.Windows.Forms.Button buttonDisable;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button buttonCheckDevices;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Management;
+
+namespace USBPortController
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            buttonCheckDevices.Click += buttonCheckDevices_Click;
+        }
+
+        private void buttonCheckDevices_Click(object sender, EventArgs e)
+        {
+            LoadUSBDevices();
+        }
+
+        private void LoadUSBDevices()
+        {
+            try
+            {
+                listBox1.Items.Clear();
+
+                ManagementObjectSearcher searcher = new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_USBHub");
+
+                foreach (ManagementObject usbDevice in searcher.Get())
+                {
+                    string deviceName = usbDevice["Caption"].ToString();
+                    listBox1.Items.Add(deviceName);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hata oluştu: " + ex.Message, "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void buttonEnable_Click(object sender, EventArgs e)
+        {
+            EnableUSBDevice();
+        }
+
+        private void buttonDisable_Click(object sender, EventArgs e)
+        {
+            DisableUSBDevice();
+        }
+
+        private void EnableUSBDevice()
+        {
+            if (listBox1.SelectedIndex >= 0)
+            {
+                string selectedDevice = listBox1.SelectedItem.ToString();
+                string deviceName = selectedDevice.Split('-')[0].Trim();
+                ProcessStartInfo startInfo = new ProcessStartInfo("\"C:\\Program Files (x86)\\Windows Kits\\10\\Tools\\10.0.22621.0\\x64\\devcon.exe\"", $"enable \"{deviceName}\"");
+                startInfo.Verb = "runas";
+                startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+
+                using (Process process = new Process())
+                {
+                    process.StartInfo = startInfo;
+                    process.Start();
+                    process.WaitForExit();
+                }
+
+                MessageBox.Show("USB cihazı başarıyla etkinleştirildi.", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Lütfen etkinleştirmek istediğiniz USB cihazını seçin.", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void DisableUSBDevice()
+        {
+            if (listBox1.SelectedIndex >= 0)
+            {
+                string selectedDevice = listBox1.SelectedItem.ToString();
+                string deviceName = selectedDevice.Split('-')[0].Trim();
+                ProcessStartInfo startInfo = new ProcessStartInfo("\"C:\\Program Files (x86)\\Windows Kits\\10\\Tools\\10.0.22621.0\\x64\\devcon.exe\"", $"disable \"{deviceName}\"");
+                startInfo.Verb = "runas";
+                startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+
+                using (Process process = new Process())
+                {
+                    process.StartInfo = startInfo;
+                    process.Start();
+                    process.WaitForExit();
+                }
+
+                MessageBox.Show("USB cihazı başarıyla devre dışı bırakıldı.", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Lütfen devre dışı bırakmak istediğiniz USB cihazını seçin.", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+    }
+}
